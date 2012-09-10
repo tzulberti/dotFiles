a9ec2ed0cd5e603fc1a4c4f06182d1f1b6e31b01
Tomas Zulberti <tzulberti@gmail.com>
2012-09-10 12:03:32 -0300

    Se agregaron nuevos aliases

---
+++ svn-color.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+"""
+Author: Saophalkun Ponlu (http://phalkunz.com)
+Contact: phalkunz@gmail.com
+Date: May 23, 2009
+Modified: June 15, 2009
+
+Additional modifications:
+Author: Phil Christensen (http://bubblehouse.org)
+Contact: phil@bubblehouse.org
+Date: February 22, 2010
+
+Requiere de colorama
+"""
+
+from colorama import init, Fore, Back, Style
+import sys, re, subprocess
+
+init()
+
+tabsize = 4
+
+colorizedSubcommands = (
+    'st',
+    'status',
+    'add',
+    'remove',
+    'diff',
+)
+
+statusColors = {
+    "M" : Fore.RED, # red
+    "\?" : Fore.BLUE, # grey
+    "A" : Fore.GREEN, # green
+    "X" : Fore.YELLOW, # yellow
+    "C" : Back.BLACK + Fore.RED , # black on red
+    "-" : Fore.GREEN, # red
+    "D" : Style.DIM + Fore.RED, # bold red
+    "\+" : Fore.GREEN, # green
+    "!": Fore.CYAN,
+}
+
+
+
+
+
+
+def colorize(line):
+    for color in statusColors:
+        if re.match(color, line):
+            print statusColors[color] + line
+            return
+    else:
+        print line
+
+def escape(s):
+    s = s.replace('$', r'\$')
+    s = s.replace('"', r'\"')
+    s = s.replace('`', r'\`')
+    return s
+
+
+
+passthru = lambda x: x
+quoted = lambda x: '"%s"' % escape(x)
+
+if __name__ == "__main__":
+    cmd = ' '.join(['svn']+[(passthru, quoted)[' ' in arg](arg) for arg in sys.argv[1:]])
+    output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    cancelled = False
+    for line in output.stdout:
+        line = line.expandtabs(tabsize)
+        line = line.strip()
+        if (sys.argv[1] in colorizedSubcommands):
+            line = colorize(line)
+        else:
+            print line
+
