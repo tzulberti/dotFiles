784917aa1ad0132a43453e11a07071ba66b28cc9
Tomas Zulberti <tzulberti@gmail.com>
2015-11-05 21:22:20 -0300

    Se actualizo el script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 sudo apt-get update
-sudo apt-get install -y chromium-browser
 sudo apt-get install -y htop vim exuberant-ctags unzip unrar
 
 # todas las cosas que necesito para poder trabajar en los diferentes
@@ -18,31 +17,19 @@ sudo apt-get install -y postgresql libpq-dev pgadmin3
 # se encarga de instalar todo lo necesario para que me funcionen los diferentes proyectos de wine
 sudo apt-get install -y ia32-libs-multiarch libtxc-dxtn-s2tc0:i386 wine
 
-# configuro el virtualbox
-sudo echo "deb http://download.virtualbox.org/virtualbox/debian saucy contrib" > /etc/apt/sources.list
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
-
-sudo apt-get update
-sudo apt-get install -y virtualbox-4.3
 
 
-sudo mkdir /media/data
-sudo mkdir /media/otros
-sudo echo "/dev/sda5	/media/data	ext4	rw,nosuid,nodev,uhelper=udisks2" > /etc/fstab
-sudo echo "/dev/sda6	/media/otros	ext4	rw,nosuid,nodev,uhelper=udisks2" > /etc/fstab
 
-sudo mount /media/data
-sudo mount /media/otros
 
 # ahora estas cosas las hago para poder tener configurado el bash como a mi me gusta
 cd /media/data/Proyectos/dotFiles
-ln -s `pwd`/bashrc ~/.bashrc
-ln -s `pwd`/vimrc ~/.vimrc
-ln -s `pwd`/bash_aliases ~/.bash_aliases
-ln -s `pwd`/gitconfig ~/.gitconfig
-ln -s `pwd`/pydistutils.cfg ~/.pydistutils.cfg
-ln -s `pwd`/screenrc ~/.screenrc
-ln -s `pwd`/sqliterc ~/.sqliterc
+ln -sf $(pwd)/bashrc ~/.bashrc
+ln -sf $(pwd)/vimrc ~/.vimrc
+ln -sf $(pwd)/bash_aliases ~/.bash_aliases
+ln -sf $(pwd)/gitconfig ~/.gitconfig
+ln -sf $(pwd)/pydistutils.cfg ~/.pydistutils.cfg
+ln -sf $(pwd)/screenrc ~/.screenrc
+ln -sf $(pwd)/sqliterc ~/.sqliterc
 
 
 # crear los virtuale-envs correspondientes
@@ -54,7 +41,4 @@ cd /media/data/Proyectos/entrenamiento-arqueria
 # esto lo tengo que hacer para todos los proyectos
 
 
-# TODO falta el jDownloader y java, y go
-sudo apt-get -y install software-properties-common
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get -y install oracle-java7-installer
+# TODO falta el jDownloader y java, y go, rust
